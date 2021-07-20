e1e98facbefba0347bc8f54bf320911efbf957d1
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2021-07-20 14:08:13 +0200

    no message

--- resources/vue/result.vue
+++ resources/vue/result.vue
@@ -1,7 +1,15 @@
 <template>
         <tr>
-       {{ result.rank }} {{ runner.name }} {{ result.start }} {{ runner.club ? "club" : "" }} {{ result.radio1 }} 
-       {{ result.radio2 }} {{ result.radio3 }} {{ result.radio4 }} {{ result.time }} {{ result.behind }}
+            <td class="w-24 pr-4 text-right">{{ result.rank ? result.rank + '.' : "" }}</td>
+            <td>{{ runner.name }}</td>
+            <td>{{ result.start }}</td>
+            <td>{{ runner.club ? "club" : "" }}</td>
+            <td>{{ result.radio1 }}</td>
+            <td>{{ result.radio2 }}</td>
+            <td>{{ result.radio3 }}</td>
+            <td>{{ result.radio4 }}</td>
+            <td>{{ result.time }}</td>
+            <td>{{ result.behind }}</td>
        </tr>
 </template>
 
