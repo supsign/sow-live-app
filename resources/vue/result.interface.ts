cc1b56218219ee1b04c18caed4b790e339f50f58
Matthias Baumgartner <matthias@zehnders.ch>
2021-07-20 13:49:46 +0200

    validRsults

---
+++ resources/vue/result.interface.ts
@@ -0,0 +1,14 @@
+export interface IResult {
+    id: number;
+    rank: number;
+    radio1: string;
+    radio2: string;
+    radio3: string;
+    radio4: string;
+    radio5: string;
+    start: string;
+    time: string;
+    behind: string;
+    runner_id: number;
+    stage_id: number;
+}
--- resources/vue/results.vue
+++ resources/vue/results.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-base text-gray-900 bg-white rounded-lg shadow-xl">
         <vue-result
-            v-for="result in results"
+            v-for="result in resultsValidRank"
             :key="result.id"
             :result="result"
             :runner="runnerByResult(result)"
@@ -14,6 +14,7 @@
 import { Component, Prop } from "vue-property-decorator";
 import Vue from "vue";
 import VueResult from "./result.vue";
+import { IResult } from "./result.interface";
 
 @Component({
     components: {
@@ -29,12 +30,17 @@ export default class VueResults extends Vue {
     @Prop({
         type: Array
     })
-    public results: Array<any>;
+    public results: Array<IResult>;
 
     @Prop({
         type: Array
     })
     public starts: Array<any>;
+
+    public get resultsValidRank() {
+        const validResults = this.results.filter(result => !!result.rank);
+        return validResults.sort((a, b) => a.rank - b.rank);
+    }
 
     public runnerByResult(result: any) {
         return this.runners.find(runner => runner.id === result.runner_id);
