8de2dedbba56eee7fddb2efcebe3832b370d801c
Matthias Baumgartner <matthias@zehnders.ch>
2021-07-20 13:20:32 +0200

    added VueResult

---
+++ resources/vue/result.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-base text-gray-900 bg-white rounded-lg shadow-xl">
+        {{ runner.name }}
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop } from "vue-property-decorator";
+import Vue from "vue";
+
+@Component
+export default class VueResult extends Vue {
+    @Prop({
+        type: Object
+    })
+    public runner: any;
+
+    @Prop({
+        type: Object
+    })
+    public result: any;
+}
+</script>
--- resources/vue/results.vue
+++ resources/vue/results.vue
@@ -1,28 +1,43 @@
 <template>
     <div class="text-base text-gray-900 bg-white rounded-lg shadow-xl">
-        {{ results }}
+        <vue-result
+            v-for="result in results"
+            :key="result.id"
+            :result="result"
+            :runner="runnerByResult(result)"
+        >
+        </vue-result>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop } from "vue-property-decorator";
 import Vue from "vue";
+import VueResult from "./result.vue";
 
-@Component
+@Component({
+    components: {
+        VueResult
+    }
+})
 export default class VueResults extends Vue {
     @Prop({
         type: Array
     })
-    runners: any;
+    public runners: Array<any>;
 
     @Prop({
         type: Array
     })
-    results: any;
+    public results: Array<any>;
 
     @Prop({
         type: Array
     })
-    starts: any;
+    public starts: Array<any>;
+
+    public runnerByResult(result: any) {
+        return this.runners.find(runner => runner.id === result.runner_id);
+    }
 }
 </script>
